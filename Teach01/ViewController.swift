3d539d42f5702425fb613547eeb8b1034cb93459
Brown <bite6210@gmail.com>
2018-05-07 11:16:10 +0700

    Initial Commit

---
+++ Teach01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teach01
+//
+//  Created by Phuket Makers on 7/5/2561 BE.
+//  Copyright © 2561 Phuket Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
